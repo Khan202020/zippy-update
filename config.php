3907e04349219604bfb536f29e102633f8937dc8
Khan202020 <100871279+Khan202020@users.noreply.github.com>
2022-04-05 14:36:03 +0530

    Update config.php
    Changes credentials of database

--- config.php
+++ config.php
@@ -1,13 +1,9 @@
 <?php
-    // $servername = "s465z7sj4pwhp7fn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-    // $username = "o1j2kuvlr36u1qbj";
-    // $password = "vijrdis04xp94xje";
-    // $dbname = "uzkpv5dhzbyyet3p";
+     $servername = "s465z7sj4pwhp7fn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+     $username = "o1j2kuvlr36u1qbj";
+     $password = "vijrdis04xp94xje";
+     $dbname = "uzkpv5dhzbyyet3p";
 
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "zippyusedautos";
 
     $conn = new mysqli($servername, $username, $password, $dbname);
 
